36422420f8fe35bd0980799b103d63998f92de23
Pablo Alecio <paleciop@hotmail.com>
2011-10-18 18:27:49 -0600

    Initialized repository

---
+++ src/digithings/android/superfacil/SuperFacilMain.java
@@ -0,0 +1,14 @@
+package digithings.android.superfacil;
+
+import android.os.Bundle;
+
+import com.phonegap.*;
+
+public class SuperFacilMain extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
